56f5b0602b0c256af2c1a10eb307c65d87a01b44
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2021-10-03 19:40:26 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://AdminKamilo:I1udrg12@cluster0.8from.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const app = express();
+const PORT = 8080;
+const fs = require("fs");
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`it's alive on http://localhost:${PORT}`);
+});
+
+app.post("/id", async (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    console.log("No id provided");
+    res.status(418).send({
+      error: "Podaj id",
+    });
+  } else {
+    const clientDb = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+    }).catch((err) => {
+      console.log(err);
+    });
+    if (!clientDb) {
+      return;
+    }
+    try {
+      const db = clientDb.db("margo");
+      const collection = db.collection("player");
+      const query = { id: `${id}` };
+      const checkID = await collection.findOne(query);
+      if (!checkID) {
+        console.log(`Klient o id: ${id} brak licencji`);
+        res.send({
+          type: "nolic",
+        });
+      } else {
+        fs.readFile(`./newBot.js`, (error, content) => {
+          if (error) {
+            res.end("File not found");
+            return;
+          }
+          res.writeHead(200, {
+            "Content-Type": `application/javascript`,
+          });
+          res.end(content, "utf-8");
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      clientDb.close();
+    }
+  }
+});
