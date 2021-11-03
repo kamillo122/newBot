8cec7658180eee0ea77023b746e532e327b966c9
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2021-11-03 15:45:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const fs = require("fs");
 const uri =
-  "mongodb+srv://AdminKamilo:I1udrg12@cluster0.8from.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+	"mongodb+srv://AdminKamilo:I1udrg12@cluster0.8from.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -11,51 +11,47 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`it's alive on http://localhost:${PORT}`);
+	console.log(`it's alive on http://localhost:${PORT}`);
 });
 
-app.post("/id", async (req, res) => {
-  const { id } = req.body;
-  if (!id) {
-    console.log("No id provided");
-    res.status(418).send({
-      error: "Podaj id",
-    });
-  } else {
-    const clientDb = await MongoClient.connect(uri, {
-      useNewUrlParser: true,
-    }).catch((err) => {
-      console.log(err);
-    });
-    if (!clientDb) {
-      return;
-    }
-    try {
-      const db = clientDb.db("margo");
-      const collection = db.collection("player");
-      const query = { id: `${id}` };
-      const checkID = await collection.findOne(query);
-      if (!checkID) {
-        console.log(`Klient o id: ${id} brak licencji`);
-        res.send({
-          type: "nolic",
-        });
-      } else {
-        fs.readFile(`./newBot.js`, (error, content) => {
-          if (error) {
-            res.end("File not found");
-            return;
-          }
-          res.writeHead(200, {
-            "Content-Type": `application/javascript`,
-          });
-          res.end(content, "utf-8");
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      clientDb.close();
-    }
-  }
+app.post("/login", async (req, res) => {
+	const { login, password } = req.body;
+	if (!login || !password) {
+		console.log("No login data provided");
+		res.status(418).send({
+			error: "Podaj dane",
+		});
+	}
+	console.log(login, password);
+	const clientDb = await MongoClient.connect(uri, {
+		useNewUrlParser: true,
+	}).catch((err) => {
+		console.log(err);
+	});
+	if (!clientDb) {
+		res.send({
+			error: "Database error connecting!",
+		});
+	}
+	try {
+		const db = await clientDb.db("snake");
+		const collection = await db.collection("snakePlayers");
+		const query = { login: login };
+		const checkID = await collection.findOne(query);
+		if (!checkID) {
+			res.send({
+				error: "login error",
+			});
+		} else {
+			res.end(
+				JSON.stringify({
+					ok: 1,
+				})
+			);
+		}
+	} catch (err) {
+		console.log(err);
+	} finally {
+		await clientDb.close();
+	}
 });
